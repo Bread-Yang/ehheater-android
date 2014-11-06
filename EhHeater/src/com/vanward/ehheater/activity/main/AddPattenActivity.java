1ac543ffb81f32585ab23f0dbdf37ccecaf979c8
zhxu <zhxu@xtremeprog.com>
2014-11-07 04:44:48 +0800

    update add pattern

--- EhHeater/src/com/vanward/ehheater/activity/main/AddPattenActivity.java
+++ EhHeater/src/com/vanward/ehheater/activity/main/AddPattenActivity.java
@@ -1,6 +1,8 @@
 package com.vanward.ehheater.activity.main;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.annotation.view.ViewInject;
@@ -63,20 +65,26 @@ public class AddPattenActivity extends EhHeaterBaseActivity implements
 	RelativeLayout peopleNum;
 	private CustomSetVo oldcustomSetVo;
 	
+	static Map<Integer,Integer> tempMap = new HashMap<Integer,Integer>();
+	
+	static{
+		tempMap.put(1, 45);
+		tempMap.put(2, 65);
+		tempMap.put(3, 75);
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_pattern);
 		FinalActivity.initInjectedView(this);
-		ivTitleName.setText("添加模式");
+		
 		ivTitleBtnLeft.setBackgroundResource(R.drawable.icon_back);
 		ivTitleBtnRigh.setBackgroundResource(R.drawable.menu_icon_ye);
 		seekBar.setOnSeekBarChangeListener(this);
-		degree.setText("35℃");
-		swich.setTag(1);
+		
 		setData();
-		peopleNum.setVisibility(View.GONE);
+		
 		peopleGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
@@ -89,15 +97,15 @@ public class AddPattenActivity extends EhHeaterBaseActivity implements
 				case R.id.RadioButton05:
 					// lock
 					// set args
-					lockTempAndPower();
+//					lockTempAndPower();
 					setArgsForTempAndPower(45, 3);
 					break;
 				case R.id.RadioButton04:
-					lockTempAndPower();
+//					lockTempAndPower();
 					setArgsForTempAndPower(65, 3);
 					break;
 				case R.id.RadioButton06:
-					lockTempAndPower();
+//					lockTempAndPower();
 					setArgsForTempAndPower(75, 3);
 					break;
 				}
@@ -105,14 +113,36 @@ public class AddPattenActivity extends EhHeaterBaseActivity implements
 		});
 	}
 	
-	public void setData(){
-	     String name=	getIntent().getStringExtra("index");
-	     if (name!=null) {
-	    	    oldcustomSetVo=	DBService.getInstance(this).findById(name, CustomSetVo.class);
-	  	     nameedittext.setText(oldcustomSetVo.getName());
-	  	   setArgsForTempAndPower(oldcustomSetVo.getTempter(),oldcustomSetVo.getPower());
+	public void setData() {
+		String name = getIntent().getStringExtra("index");
+		if (name != null) {
+			oldcustomSetVo = DBService.getInstance(this).findById(name, CustomSetVo.class);
+			
+			nameedittext.setText(oldcustomSetVo.getName());
+			
+			if( oldcustomSetVo.getPeoplenum() > 0 ){
+				for (int i = 0; i < peopleGroup.getChildCount(); i++) {
+					if ( Integer.parseInt(peopleGroup.getChildAt(i).getTag().toString()) == oldcustomSetVo.getPeoplenum()) {
+						((RadioButton)peopleGroup.getChildAt(i)).setChecked(true);
+						break;
+					}
+				}
+				setSwich(true);
+			}else{
+				setArgsForTempAndPower(oldcustomSetVo.getTempter(),oldcustomSetVo.getPower());
+				setSwich(false);
+			}
+			
+			ivTitleName.setText("编辑模式");
+			
+		}else{
+			ivTitleName.setText("添加模式");
+			degree.setText("35℃");
+			swich.setTag(1);
+			peopleNum.setVisibility(View.GONE);
+			
 		}
-	  
+
 	}
 
 	private void lockTempAndPower() {
@@ -153,20 +183,41 @@ public class AddPattenActivity extends EhHeaterBaseActivity implements
 			return null;
 		}
 		customSetVo.setName(nameedittext.getText().toString());
-		customSetVo.setTempter(seekBar.getProgress() + 35);
-		List list = new BaseDao(this).getDb().findAll(CustomSetVo.class);
-		View view = null;
-		for (int i = 0; i < powerGroup.getChildCount(); i++) {
-			if (powerGroup.getCheckedRadioButtonId() == powerGroup
-					.getChildAt(i).getId()) {
-				view = powerGroup.getChildAt(i);
+		
+		if( swich.getTag() != null && Integer.parseInt(swich.getTag().toString()) == 0 ){
+			
+			View view = null;
+			for (int i = 0; i < peopleGroup.getChildCount(); i++) {
+				if (peopleGroup.getCheckedRadioButtonId() == peopleGroup.getChildAt(i).getId()) {
+					view = peopleGroup.getChildAt(i);
+				}
 			}
+			
+			customSetVo.setPeoplenum(Integer.parseInt(view.getTag().toString()));
+			customSetVo.setTempter(tempMap.get(customSetVo.getPeoplenum()));
+			customSetVo.setPower(3);
+			
+			
+		}else{
+			
+			customSetVo.setTempter(seekBar.getProgress() + 35);
+			List list = new BaseDao(this).getDb().findAll(CustomSetVo.class);
+			View view = null;
+			for (int i = 0; i < powerGroup.getChildCount(); i++) {
+				if (powerGroup.getCheckedRadioButtonId() == powerGroup
+						.getChildAt(i).getId()) {
+					view = powerGroup.getChildAt(i);
+				}
+			}
+			System.out.println("power:　" + view.getTag());
+			customSetVo.setPower(Integer.parseInt((String) view.getTag()));
+			
 		}
-		System.out.println("power:　" + view.getTag());
-		customSetVo.setPower(Integer.parseInt((String) view.getTag()));
 		return customSetVo;
 	}
 
+	
+	
 	@Override
 	public void onClick(View view) {
 		switch (view.getId()) {
@@ -185,19 +236,7 @@ public class AddPattenActivity extends EhHeaterBaseActivity implements
 
 			break;
 		case R.id.imageButton1:
-			if ((Integer) swich.getTag() == 1) {
-				peopleNum.setVisibility(View.VISIBLE);
-				temlayout.setVisibility(View.GONE);
-				powerLayout.setVisibility(View.GONE);
-				swich.setImageResource(R.drawable.on);
-				swich.setTag(0);
-			} else {
-				peopleNum.setVisibility(View.GONE);
-				temlayout.setVisibility(View.VISIBLE);
-				powerLayout.setVisibility(View.VISIBLE);
-				swich.setImageResource(R.drawable.off);
-				swich.setTag(1);
-			}
+			setSwich((Integer) swich.getTag() == 1);
 			break;
 		default:
 			break;
@@ -205,6 +244,22 @@ public class AddPattenActivity extends EhHeaterBaseActivity implements
 		super.onClick(view);
 	}
 
+	public void setSwich(boolean plag){
+		if( plag ){
+			peopleNum.setVisibility(View.VISIBLE);
+			temlayout.setVisibility(View.GONE);
+			powerLayout.setVisibility(View.GONE);
+			swich.setImageResource(R.drawable.on);
+			swich.setTag(0);
+		}else{
+			peopleNum.setVisibility(View.GONE);
+			temlayout.setVisibility(View.VISIBLE);
+			powerLayout.setVisibility(View.VISIBLE);
+			swich.setImageResource(R.drawable.off);
+			swich.setTag(1);
+		}
+	}
+	
 	@Override
 	public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
 		degree.setText(arg1 + 35 + "℃");
